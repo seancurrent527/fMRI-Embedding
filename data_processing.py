cee596c6896559e883d51544008c3a0bf31acb03
Sean Current <seancurrent527@email.arizona.edu>
2020-10-06 22:02:23 -0400

    Initial data preprocessing for adjacency matrices.

---
+++ data_processing.py
@@ -0,0 +1,27 @@
+from scipy.io import loadmat
+import numpy as np
+
+def read_data(Xfile, yfile):
+    X = loadmat(Xfile)['r']
+    y = loadmat(yfile)['t']
+    return X.T, y
+
+def adjacency_matrix(X):
+    adjacent_X = np.ones((len(X), 268, 268))
+    index = 0
+    for i in range(268):
+        for j in range(i):
+            adjacent_X[:, i, j] = X[:, index]
+            adjacent_X[:, j, i] = X[:, index]
+            index += 1
+    return adjacent_X
+
+#============================================
+def main():
+    X, y = read_data('fake_data_unique.mat', 'fake_targetvariable.mat')
+    X = adjacency_matrix(X)
+
+    print(X)
+
+if __name__ == '__main__':
+    main()
