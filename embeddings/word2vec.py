0029eebd1361bfc5dfb121845d4394bc2db29224
mattRiffle7 <riffle.94@osu.edu>
2020-10-09 19:46:34 -0400

    added choice between skip-gram and BOW

--- embeddings/word2vec.py
+++ embeddings/word2vec.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sys, os
+import argparse
 sys.path.insert(0, '.')
 
 import data_processing
@@ -15,14 +16,6 @@ class Word2Vec:
         self.encoder = np.random.uniform(size = (self.vocab_size, self.embedding_dim))
         self.decoder = np.random.uniform(size = (self.embedding_dim, self.vocab_size))
 
-    def encode(self, word):
-        embedding = word @ self.encoder
-        return embedding
-
-    def decode(self, embedding):
-        output = embedding @ self.decoder
-        return output
-
     def softmax(self, output):
         probabilities = np.exp(output) / np.exp(output).sum()
         return probabilities
@@ -33,12 +26,6 @@ class Word2Vec:
         probabilities = self.softmax(output)
         return probabilities, embedding, output
 
-    def error(self, probabilities, context):
-        return (probabilities - context).sum(axis = 0)
-
-    def loss(self, output, context):
-        return (-output[context.argmax(axis = 1)].sum() + len(context) * np.log(np.sum(np.exp(output))))
-
     def backprop(self, err, embedding, word):
         grad_decoder = np.outer(embedding, err)
         grad_encoder = np.outer(word, (self.decoder @ err))
@@ -46,7 +33,26 @@ class Word2Vec:
         self.encoder -= self.learning_rate * grad_encoder
         self.decoder -= self.learning_rate * grad_decoder
 
-    def train(self, X, y, epochs = 50):
+class Skip_Gram(Word2Vec):
+    # This is a skip-gram version
+    def __init__(self, vocab_size, embedding_dim, kernel_width, learning_rate):
+        super().__init__(vocab_size, embedding_dim, kernel_width, learning_rate)
+
+    def encode(self, word):
+        embedding = word @ self.encoder
+        return embedding
+
+    def decode(self, embedding):
+        output = embedding @ self.decoder
+        return output
+
+    def error(self, probabilities, context):
+        return (probabilities - context).sum(axis = 0)
+
+    def loss(self, output, context):
+        return (-output[context.argmax(axis = 1)].sum() + len(context) * np.log(np.sum(np.exp(output))))
+
+    def train(self, X, y, epochs=50):
         for i in range(epochs):
             los = 0
             for word, context in zip(X, y):
@@ -56,10 +62,10 @@ class Word2Vec:
                 self.backprop(err, embedding, word)
 
                 los += self.loss(output, context)
-            
+
             print(f'Epoch {i + 1}: loss = {los}')
 
-    def train_from_seq(self, seq, epochs = 50):
+    def train_from_seq(self, seq, epochs=50):
         # seq should be a sequence of one-hot vectors
         ordering = np.random.permutation(len(seq) - (2 * self.kernel_width)) + self.kernel_width
         X, y = [], []
@@ -69,10 +75,61 @@ class Word2Vec:
             X.append(word)
             y.append(context)
 
-        self.train(X, y, epochs = epochs)
+        self.train(X, y, epochs=epochs)
+
+class CBOW(Word2Vec):
+    # This is a contiuous-bag-of-words version
+    def __init__(self, vocab_size, embedding_dim, kernel_width, learning_rate):
+        super().__init__(vocab_size, embedding_dim, kernel_width, learning_rate)
+
+    def encode(self, seq):
+        embedding = seq @ self.encoder
+        return embedding
+
+    def decode(self, seq):
+        output = seq @ self.decoder
+        return output
+
+    def error(self, probabilities, word):
+        return probabilities - word
+
+    def loss(self, output, word):
+        return (-output[word == 1] + np.log(np.sum(np.exp(output))))[0]
+
+    def train(self, X, y, epochs=50):
+        for i in range(epochs):
+            los = 0
+            for seq, word in zip(X, y):
+                seq = seq.mean(axis=0)
+                probabilities, embedding, output = self.forward(seq)
+                err = self.error(probabilities, word)
+
+                self.backprop(err, embedding, seq)
+
+                los += self.loss(output, word)
+
+            print(f'Epoch {i + 1}: loss = {los}')
+
+    def train_from_seq(self, seq, epochs=50):
+        # seq should be a sequence of one-hot vectors
+        ordering = np.random.permutation(len(seq) - (2 * self.kernel_width)) + self.kernel_width
+        X, y = [], []
+        for i in ordering:
+            word = seq[i]
+            context = np.concatenate([seq[i - self.kernel_width: i], seq[i + 1: i + 1 + self.kernel_width]])
+            X.append(context)
+            y.append(word)
+
+        self.train(X, y, epochs=epochs)
 
 #=============================================
 def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v')
+    args = parser.parse_args()
+    version = int(args.v)
+
     X, y = data_processing.read_data('fake_data_unique.mat', 'fake_targetvariable.mat')
     X = data_processing.adjacency_matrix(X)
 
@@ -81,7 +138,12 @@ def main():
     for i, pos in enumerate(walk):
         one_hot[i, pos] = 1
 
-    model = Word2Vec(268, 64, 2, 0.1)
+    if version == 0:
+        print("Skip-Gram")
+        model = Skip_Gram(268, 64, 2, 0.1)
+    else:
+        print("CBOW")
+        model = CBOW(268, 64, 2, 0.1)
     model.train_from_seq(one_hot, epochs = 100)
 
 if __name__ == '__main__':
